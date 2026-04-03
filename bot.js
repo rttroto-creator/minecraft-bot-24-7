7f30a73615131a49a82b941274dfa399831f7a08
rttroto-creator <rttroto@gmail.com>
2026-04-03 21:25:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,34 @@
+const mineflayer = require('mineflayer')
+const http = require('http')
+
+// Простий веб-сервер для Render
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('Bot is running!')
+})
+server.listen(process.env.PORT || 3000)
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    host: 'FRFRFRFRFRRFRFR.aternos.me', 
+    port: 25565,
+    username: 'SmartBot_24_7',
+    version: '1.21.1', // Спробуй цю версію, вона найстабільніша
+    checkTimeout: 15000
+  })
+
+  bot.on('spawn', () => console.log('[+] Бот у грі!'))
+  bot.on('chat', (username, message) => {
+    if (username === bot.username) return
+    if (message === 'іди сюди') bot.chat('Біжу до тебе!')
+  })
+
+  bot.on('error', (err) => console.log('Помилка:', err))
+  bot.on('end', () => {
+    console.log('Відключено. Перепідключення через 15 сек...')
+    setTimeout(createBot, 15000)
+  })
+}
+
+createBot()
 
